7bf1543f79cb895bc16829e0128dc359228962dc
biohackerellie <epkerns@gmail.com>
2023-04-04 14:08:44 -0600

    d

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -10,7 +10,7 @@ function Intro() {
       <p  className="text-sky-500 md:text-xl mb-3 font-medium">
       <Type  className="text-sky-500 md:text-xl mb-3 font-medium"/>
       </p>
-      <p className="columns-auto text-m max-w-xl mb-6 font-bold"> I'm an Audio Engineer, Artist, and Musican by hobby, and in my professional career, I've spent the last 10 years working with IT Systems and have begun moving into software development.
+      <p className="columns-auto text-m max-w-xl mb-6 font-bold"> I'm an Audio Engineer, Artist, and Musician by hobby, and in my professional career, I've spent the last 10 years working with IT Systems and have begun moving into software development.
               Basically you can call me a nerd 🤷‍♀️ <br /> <span />
 						
               My decade of IT experience includes working with Powershell, Linux, Windows Server, Docker, and more! My specialties as a Sys Admin are  Automation and Virtualization  and also in areas related to Devops and Network Administration. 
