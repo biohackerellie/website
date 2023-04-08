fa8bb4d76cf0b02bb13b6c068c3b0e3bbd5a3b3a
biohackerellie <epkerns@gmail.com>
2023-04-08 22:46:44 +0000

    contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -16,9 +16,40 @@ const Contact = () => {
   });
   const [loading, setloading] = useState(false);
 
-  const handleChange = (e) => {}
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setForm({ ...form, [name]: value})
+  }
 
-  const handleSubmit = (e) => {}
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setloading(true);
+    emailjs.send(
+      'service_71uk4cg',
+      'template_pbikspn',
+      {
+        from_name: form.name,
+        to_name: 'Ellie',
+        from_email: form.email,
+        to_email: 'ellie@epklabs.com',
+        message: form.message,
+      },
+      'Yq--SC51KxqbTrCVN'
+      )
+      .then(() => {
+        setloading(false)
+        alert('Thank you. I will get in touch as soon as possible!')
+        setForm=({
+          name: '',
+          email: '',
+          message: '',
+        })
+      }, (error) => {
+        setloading(false)
+        console.log(error);
+        alert('Something went wrong.')
+      })
+  }
 
   return (
     <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
