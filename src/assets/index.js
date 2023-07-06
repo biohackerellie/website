3c72aef9b742bfbc69eadd11568b5cabd1eb1d44
biohackerellie <epkerns@gmail.com>
2023-07-06 17:45:39 -0600

    added facilities site to works section

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,63 +1,64 @@
-import logo from "./logo.png";
-import backend from "./backend.png";
-import creator from "./creator.png";
-import mobile from "./mobile.png";
-import web from "./web.png";
-import github from "./github.png";
-import menu from "./menu.svg";
-import close from "./close.svg";
-import me from "./Me.png";
-import css from "./tech/css.png";
-import docker from "./tech/docker.png";
-import bash from "./tech/bash.png";
-import git from "./tech/git.png";
-import html from "./tech/html.png";
-import javascript from "./tech/javascript.png";
-import ableton from "./tech/ableton.png";
-import nodejs from "./tech/nodejs.png";
-import reactjs from "./tech/reactjs.png";
-import python from "./tech/python.png";
-import tailwind from "./tech/tailwind.png";
-import typescript from "./tech/typescript.png";
-import ruby from "./tech/ruby.png";
-import terminal from "./terminal.png"
-import bb from "./company/bb.png";
-import cpr from "./company/cpr.png";
-import laurel from "./company/laurel.png";
-
-import docs from "./docs.png";
-import rds from "./rds.png";
-import app4 from "./app4.png";
-import Site from "./Site.png";
+import logo from './logo.png';
+import backend from './backend.png';
+import creator from './creator.png';
+import mobile from './mobile.png';
+import web from './web.png';
+import github from './github.png';
+import menu from './menu.svg';
+import close from './close.svg';
+import me from './Me.png';
+import css from './tech/css.png';
+import docker from './tech/docker.png';
+import bash from './tech/bash.png';
+import git from './tech/git.png';
+import html from './tech/html.png';
+import javascript from './tech/javascript.png';
+import ableton from './tech/ableton.png';
+import nodejs from './tech/nodejs.png';
+import reactjs from './tech/reactjs.png';
+import python from './tech/python.png';
+import tailwind from './tech/tailwind.png';
+import typescript from './tech/typescript.png';
+import ruby from './tech/ruby.png';
+import terminal from './terminal.png';
+import bb from './company/bb.png';
+import cpr from './company/cpr.png';
+import laurel from './company/laurel.png';
+import facilities from './facilities.png';
+import docs from './docs.png';
+import rds from './rds.png';
+import app4 from './app4.png';
+import Site from './Site.png';
 export {
 	me,
-  bb,
-  cpr,
-  laurel,
-  logo,
-  backend,
-  creator,
-  mobile,
-  web,
-  github,
-  menu,
-  close,
-  css,
-  docker,
-  bash,
-  git,
-  html,
-  javascript,
-  ableton,
-  nodejs,
-  reactjs,
-  python,
-  tailwind,
-  typescript,
-  ruby,
-  docs,
-  rds,
-  app4,
+	bb,
+	cpr,
+	laurel,
+	logo,
+	backend,
+	creator,
+	mobile,
+	web,
+	github,
+	menu,
+	close,
+	css,
+	facilities,
+	docker,
+	bash,
+	git,
+	html,
+	javascript,
+	ableton,
+	nodejs,
+	reactjs,
+	python,
+	tailwind,
+	typescript,
+	ruby,
+	docs,
+	rds,
+	app4,
 	terminal,
-	Site
+	Site,
 };
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,278 +1,298 @@
 import {
-  mobile,
-  backend,
-  creator,
-  web,
-  javascript,
-  typescript,
-  html,
-  css,
-  reactjs,
-  bash,
-  tailwind,
-  nodejs,
-  ableton,
-  git,
-  ruby,
-  docker,
-  bb,
-  cpr,
-  laurel,
-  docs,
-  rds,
-  app4,
-  python,
+	mobile,
+	backend,
+	creator,
+	web,
+	javascript,
+	typescript,
+	html,
+	css,
+	reactjs,
+	bash,
+	tailwind,
+	nodejs,
+	ableton,
+	git,
+	ruby,
+	docker,
+	bb,
+	cpr,
+	laurel,
+	docs,
+	facilities,
+	rds,
+	app4,
+	python,
 	terminal,
-	Site
-} from "../assets";
+	Site,
+} from '../assets';
 
 export const navLinks = [
-  {
-    id: "about",
-    title: "About",
-  },
-  {
-    id: "work",
-    title: "Work",
-  },
-  {
-    id: "contact",
-    title: "Contact",
-  },
-
+	{
+		id: 'about',
+		title: 'About',
+	},
+	{
+		id: 'work',
+		title: 'Work',
+	},
+	{
+		id: 'contact',
+		title: 'Contact',
+	},
 ];
 
 const services = [
-  {
-    title: "Web Developer",
-    icon: web,
-  },
-  {
-    title: "React Native Developer",
-    icon: mobile,
-  },
-  {
-    title: "Systems Administrator",
-    icon: backend,
-  },
-  {
-    title: "Linux Administrator",
-    icon: creator,
-  },
+	{
+		title: 'Web Developer',
+		icon: web,
+	},
+	{
+		title: 'React Native Developer',
+		icon: mobile,
+	},
+	{
+		title: 'Systems Administrator',
+		icon: backend,
+	},
+	{
+		title: 'Linux Administrator',
+		icon: creator,
+	},
 ];
 
 const technologies = [
-  {
-    name: "HTML 5",
-    icon: html,
-  },
-  {
-    name: "CSS 3",
-    icon: css,
-  },
-  {
-    name: "JavaScript",
-    icon: javascript,
-  },
-  {
-    name: "TypeScript",
-    icon: typescript,
-  },
-  {
-    name: "React JS",
-    icon: reactjs,
-  },
-  {
-    name: "Bash",
-    icon: bash,
-  },
-  {
-    name: "Tailwind CSS",
-    icon: tailwind,
-  },
-  {
-    name: "Node JS",
-    icon: nodejs,
-  },
-  {
-    name: "Ableton",
-    icon: ableton,
-  },
-  {
-    name: "Python",
-    icon: python,
-  },
-  {
-    name: "git",
-    icon: git,
-  },
-  {
-    name: "Ruby",
-    icon: ruby,
-  },
-  {
-    name: "docker",
-    icon: docker,
-  },
+	{
+		name: 'HTML 5',
+		icon: html,
+	},
+	{
+		name: 'CSS 3',
+		icon: css,
+	},
+	{
+		name: 'JavaScript',
+		icon: javascript,
+	},
+	{
+		name: 'TypeScript',
+		icon: typescript,
+	},
+	{
+		name: 'React JS',
+		icon: reactjs,
+	},
+	{
+		name: 'Bash',
+		icon: bash,
+	},
+	{
+		name: 'Tailwind CSS',
+		icon: tailwind,
+	},
+	{
+		name: 'Node JS',
+		icon: nodejs,
+	},
+	{
+		name: 'Ableton',
+		icon: ableton,
+	},
+	{
+		name: 'Python',
+		icon: python,
+	},
+	{
+		name: 'git',
+		icon: git,
+	},
+	{
+		name: 'Ruby',
+		icon: ruby,
+	},
+	{
+		name: 'docker',
+		icon: docker,
+	},
 ];
 
 const experiences = [
-  {
-    title: "Geek Squad Agent, Home Theater Installer",
-    company_name: "Best Buy",
-    icon: bb,
-    iconBg: "#00385f",
-    date: "2013 - 2016",
-    points: [
-      "Provided Customer Service to advanced level issues in store and in home.",
-      "Worked on in store device services such as data transfers and virus removals.",
-      "Installed a wide range of home theater products in customers homes.",
-    ],
-  },
-  {
-    title: "Lead Repair Technician / Store Manager",
-    company_name: "CPR Cell Phone Repair",
-    icon: cpr,
-    iconBg: "#8f0100",
-    date: "2016 - 2019",
-    points: [
-      "Managed retail and repair employees.",
-      "Performed advanced level repairs that came into our shop. I.E. Game Console repair, micro solder phone motherboard repairs ( iphone IC Chips, USB-C charging ports) and more.",
-      "Maintained inventory on repair parts, device accessories, and used devices.",
-    ],
-  },
-  {
-    title: "IT Help Desk",
-    company_name: "Laurel & Billings Public Schools",
-    icon: laurel,
-    iconBg: "#ffffff",
-    date: "2019 - 2021",
-    points: [
-      "Assisted end users with ticket requests ranging from hardware and software problems to general education on systems.",
-      "Assisted in hardware installation for new desktop and laptop stations and cat cable pulls for new networking installations.",
-      "Maintained inventory of district hardware, repair parts, and software licenses. ",
-    ],
-  },
-  {
-    title: "IT Systems Administrator",
-    company_name: "Laurel Public Schools",
-    icon: laurel,
-    iconBg: "#ffffff",
-    date: "2021 - Present",
-    points: [
-      "Lead the redesign of the school district's new website, laurel.k12.mt.us, and educated all end users on the new site management system.",
-      "Spearheaded the modernization of several district systems, such as Jira Cloud for IT and Facility project management, and SchoolSpace, for handling district facility rentals.",
-      "Developed automation scripts with Powershell, Python, and Bash for handling student and staff enrollment, and security badge access, web app development pipelines, and more.",
-      "Managed Windows and Linux based server and network environments across all district buildings.",
-      "Developed several web applications for the District including documentation sites and network monitoring systems."
-    ],
-  },
+	{
+		title: 'Geek Squad Agent, Home Theater Installer',
+		company_name: 'Best Buy',
+		icon: bb,
+		iconBg: '#00385f',
+		date: '2013 - 2016',
+		points: [
+			'Provided Customer Service to advanced level issues in store and in home.',
+			'Worked on in store device services such as data transfers and virus removals.',
+			'Installed a wide range of home theater products in customers homes.',
+		],
+	},
+	{
+		title: 'Lead Repair Technician / Store Manager',
+		company_name: 'CPR Cell Phone Repair',
+		icon: cpr,
+		iconBg: '#8f0100',
+		date: '2016 - 2019',
+		points: [
+			'Managed retail and repair employees.',
+			'Performed advanced level repairs that came into our shop. I.E. Game Console repair, micro solder phone motherboard repairs ( iphone IC Chips, USB-C charging ports) and more.',
+			'Maintained inventory on repair parts, device accessories, and used devices.',
+		],
+	},
+	{
+		title: 'IT Help Desk',
+		company_name: 'Laurel & Billings Public Schools',
+		icon: laurel,
+		iconBg: '#ffffff',
+		date: '2019 - 2021',
+		points: [
+			'Assisted end users with ticket requests ranging from hardware and software problems to general education on systems.',
+			'Assisted in hardware installation for new desktop and laptop stations and cat cable pulls for new networking installations.',
+			'Maintained inventory of district hardware, repair parts, and software licenses. ',
+		],
+	},
+	{
+		title: 'IT Systems Administrator',
+		company_name: 'Laurel Public Schools',
+		icon: laurel,
+		iconBg: '#ffffff',
+		date: '2021 - Present',
+		points: [
+			"Lead the redesign of the school district's new website, laurel.k12.mt.us, and educated all end users on the new site management system.",
+			'Spearheaded the modernization of several district systems, such as Jira Cloud for IT and Facility project management, and SchoolSpace, for handling district facility rentals.',
+			'Developed automation scripts with Powershell, Python, and Bash for handling student and staff enrollment, and security badge access, web app development pipelines, and more.',
+			'Managed Windows and Linux based server and network environments across all district buildings.',
+			'Developed several web applications for the District including documentation sites and network monitoring systems.',
+		],
+	},
 ];
-
-
 
 const projects = [
 	{
-		name: "Full Stack Music App",
+		name: 'Facility Reservation App',
 		description:
-		"A full stack music app built entirely with React, Express, and SQLite. As a musician, I wanted a site to keep track of my music and lyrics, so I built one! The app is customizable so any artist can use it to keep track of their music.",
+			"Full stack application written with Typescript using the full power of NextJs' version 13.4 App Router. Features full use of React server components, SQL database querying with Prisma, and user authentication with NextAuth",
 		tags: [
 			{
-				name: "React",
-				color: "blue-text-gradient",
+				name: 'React',
+				color: 'blue-text-gradient',
 			},
 			{
-				name: "Express",
-				color: "green-text-gradient",
+				name: 'Next JS',
+				color: 'green-text-gradient',
 			},
 			{
-				name: "SQLite",
-				color: "pink-text-gradient",
+				name: 'Typescript',
+				color: 'pink-text-gradient',
+			},
+		],
+		image: facilities,
+		source_code_link: 'https://facilities.laurel.k12.mt.us',
+	},
+	{
+		name: 'Full Stack Music App',
+		description:
+			'A full stack music app built entirely with React, Express, and SQLite. As a musician, I wanted a site to keep track of my music and lyrics, so I built one! The app is customizable so any artist can use it to keep track of their music.',
+		tags: [
+			{
+				name: 'React',
+				color: 'blue-text-gradient',
+			},
+			{
+				name: 'Express',
+				color: 'green-text-gradient',
+			},
+			{
+				name: 'SQLite',
+				color: 'pink-text-gradient',
 			},
 		],
 		image: Site,
-		source_code_link: "https://github.com/biohackerellie/music-app",
+		source_code_link: 'https://github.com/biohackerellie/music-app',
 	},
 	{
-    name: "React-Terminal",
-    description:
-      "Following the inspiration of others on the internet, I created a terminal emulator in React with support for nested commands! This project is still in development, but I plan to add more features and make it more customizable.",
-    tags: [
-      {
-        name: "React",
-        color: "blue-text-gradient",
-      },
-      {
-        name: "Tailwind CSS",
-        color: "green-text-gradient",
-      },
-      {
-        name: "Vite",
-        color: "pink-text-gradient",
-      },
-    ],
-    image: terminal,
-    source_code_link: "https://github.com/biohackerellie/react-terminal",
-  },
-  {
-    name: "Documentation Sites",
-    description:
-      "I've made several different documentation sites using React. Examples include my personal documentation as well as a site for Laurel Public Schools",
-    tags: [
-      {
-        name: "React",
-        color: "blue-text-gradient",
-      },
-      {
-        name: "Docusaurus",
-        color: "green-text-gradient",
-      },
-      {
-        name: "Docker",
-        color: "pink-text-gradient",
-      },
-    ],
-    image: docs,
-    source_code_link: "https://github.com/Laurel-Public-Schools/links",
-  },
-  {
-    name: "Windows RDS",
-    description:
-      "I designed a custom Windows RDS solution for students to use on their chromebooks with Apache Guacamole as a custom gateway and load-balancer",
-    tags: [
-      {
-        name: "Docker",
-        color: "blue-text-gradient",
-      },
-      {
-        name: "Windows RDS",
-        color: "green-text-gradient",
-      },
-      {
-        name: "Azure",
-        color: "pink-text-gradient",
-      },     
-      {
-        name: "SQL",
-        color: "purple-text-gradient",
-      },
-    ],
-    image: rds,
-    source_code_link: "https://github.com/",
-  },
-  {
-    name: "Active Directory Tools",
-    description:
-      "A Windows desktop app written in Powershell to give quick access to most used Active Directory functions such as password resets and Security Group assignments",
-    tags: [
-      {
-        name: "powershell",
-        color: "blue-text-gradient",
-      },
-    ],
-    image: app4,
-    source_code_link: "https://github.com/biohackerellie/AppDev/tree/main/Powershell/ADUserGroups",
-  },
+		name: 'React-Terminal',
+		description:
+			'Following the inspiration of others on the internet, I created a terminal emulator in React with support for nested commands! This project is still in development, but I plan to add more features and make it more customizable.',
+		tags: [
+			{
+				name: 'React',
+				color: 'blue-text-gradient',
+			},
+			{
+				name: 'Tailwind CSS',
+				color: 'green-text-gradient',
+			},
+			{
+				name: 'Vite',
+				color: 'pink-text-gradient',
+			},
+		],
+		image: terminal,
+		source_code_link: 'https://github.com/biohackerellie/react-terminal',
+	},
+	{
+		name: 'Documentation Sites',
+		description:
+			"I've made several different documentation sites using React. Examples include my personal documentation as well as a site for Laurel Public Schools",
+		tags: [
+			{
+				name: 'React',
+				color: 'blue-text-gradient',
+			},
+			{
+				name: 'Docusaurus',
+				color: 'green-text-gradient',
+			},
+			{
+				name: 'Docker',
+				color: 'pink-text-gradient',
+			},
+		],
+		image: docs,
+		source_code_link: 'https://github.com/Laurel-Public-Schools/links',
+	},
+	{
+		name: 'Windows RDS',
+		description:
+			'I designed a custom Windows RDS solution for students to use on their chromebooks with Apache Guacamole as a custom gateway and load-balancer',
+		tags: [
+			{
+				name: 'Docker',
+				color: 'blue-text-gradient',
+			},
+			{
+				name: 'Windows RDS',
+				color: 'green-text-gradient',
+			},
+			{
+				name: 'Azure',
+				color: 'pink-text-gradient',
+			},
+			{
+				name: 'SQL',
+				color: 'purple-text-gradient',
+			},
+		],
+		image: rds,
+		source_code_link: 'https://github.com/',
+	},
+	{
+		name: 'Active Directory Tools',
+		description:
+			'A Windows desktop app written in Powershell to give quick access to most used Active Directory functions such as password resets and Security Group assignments',
+		tags: [
+			{
+				name: 'powershell',
+				color: 'blue-text-gradient',
+			},
+		],
+		image: app4,
+		source_code_link:
+			'https://github.com/biohackerellie/AppDev/tree/main/Powershell/ADUserGroups',
+	},
 ];
 
 export { services, technologies, experiences, projects };
