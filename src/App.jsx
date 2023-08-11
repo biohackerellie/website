b98f89690b55a554ab1c85785ec6a79f7670bb92
biohackerellie <epkerns@gmail.com>
2023-08-11 16:19:34 -0600

    remove links link

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,40 @@
-import react from 'react';
+import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import { About, Contact, Experience, Navbar,Hero, Tech, Works, StarsCanvas, } from './components';
-import { motion } from 'framer-motion'
-import './styles/index.css'
-
+import {
+	About,
+	Contact,
+	Experience,
+	Navbar,
+	Hero,
+	Tech,
+	Works,
+	StarsCanvas,
+} from './components';
+import { motion } from 'framer-motion';
+import './styles/index.css';
 
 const App = () => {
-  return (
-		
+	return (
 		<BrowserRouter>
-
-			<motion.div 
-				className="relative z-0 bg-primary"
+			<motion.div
+				className='relative z-0 bg-primary'
 				animate={{ opacity: 1 }}
 				transition={{ from: 0, duration: 3 }}
-				>
-				<div className = "bg-hero-pattern  bg-cover bg-no-repeat bg-center">
+			>
+				<div className='bg-hero-pattern  bg-cover bg-no-repeat bg-center'>
 					<Navbar />
 					<Hero />
-					
 				</div>
 				<About />
 				<Experience />
 				<Tech />
 				<Works />
-				<div className="relative z-0">
+				<div className='relative z-0'>
 					<Contact />
-					
 				</div>
 			</motion.div>
-			
 		</BrowserRouter>
-  )
-}
+	);
+};
 
-export default App
-
-/////test
+export default App;
